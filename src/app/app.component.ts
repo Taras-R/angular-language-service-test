7aab4e53f5818c5daa2d81af88ab030fb338ac6a
Taras-R <8331527+Taras-R@users.noreply.github.com>
2023-03-13 19:16:39 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non augue
+        eu felis commodo finibus eu ut dui.
+      </p>
+    </div>
+  `
+})
+export class AppComponent {
+  title = 'angular-15.1-test';
+}
---
+++ src/app/test.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  styles: [],
+  template: `
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non augue
+        eu felis commodo finibus eu ut dui.
+      </p>
+    </div>
+  `
+})
+export class TestComponent {}
